1a83b5050066e80f0f25e01255c84cb492850b0d
Austin Wang <austinwang@Austins-MBP>
2020-01-20 22:18:47 -0500

    barcodes script

--- get_barcodes.py
+++ get_barcodes.py
@@ -2,38 +2,47 @@ import os
 import pickle
 import h5py
 
-def load_hdf5(in_file):
-    with h5py.File(in_file, "r") as hdf_in:
-        barcodes = []
-        for i in hdf_in["obs"]:
-            if isinstance(i[0], bytes):
-                entry = i[0].decode("utf-8")
-            else:
-                entry = i[0]
+# def load_hdf5(in_file):
+#     with h5py.File(in_file, "r") as hdf_in:
+#         barcodes = []
+#         for i in hdf_in["obs"]:
+#             if isinstance(i[0], bytes):
+#                 entry = i[0].decode("utf-8")
+#             else:
+#                 entry = i[0]
 
-            # print(i[0].decode("utf-8").split("-")) ####
-            try:
-                barcodes.append(entry.split("-")[0])
-            except Exception as e:
-                print(entry) ####
-                raise e
+#             # print(i[0].decode("utf-8").split("-")) ####
+#             try:
+#                 barcodes.append(entry.split("-")[0])
+#             except Exception as e:
+#                 print(entry) ####
+#                 raise e
+
+#     return barcodes
+
+def load_barcodes(in_path):
+    barcodes = {}
+    with open(in_path, "r") as in_file:
+        header = next(in_file)
+        cols = header.split(",")
+        barcode_idx = cols.index("index")
+        well_idx = cols.index("well")
+        for line in in_file:
+            bc = cols[barcode_idx].split("-")[0]
+            barcodes.setdefault(line[well_idx], []).append(bc)
 
     return barcodes
 
-def get_barcodes(in_dir, out_path):
-    in_files = [os.path.join(in_dir, i) for i in os.listdir(in_dir) if i.endswith(".h5ad")]
-    barcodes = []
-    for i in in_files:
-        barcodes.extend(load_hdf5(i))
-    barcodes.sort()
-    for ind, val in enumerate(barcodes): ####
-        if val == barcodes[ind-1]:
-            print(val)
+
+def get_barcodes(in_path, out_path):
+    barcodes = load_barcodes(in_path)
+    for v in barcodes.values():
+        v.sort()
     with open(out_path, "wb") as out_file:
         pickle.dump(barcodes, out_file)
 
 if __name__ == '__main__':
     data_dir = "/agusevlab/awang/sc_le/"
-    in_dir = os.path.join(data_dir, "genotypes")
+    in_path = os.path.join(data_dir, "adata_obs.V6.1.txt")
     out_path = os.path.join(data_dir, "barcodes.pickle")
-    get_barcodes(in_dir, out_path)
+    get_barcodes(in_path, out_path)
