2dff02a4ee5706f44af2c8e9ef7c5c075020c4fd
Austin Wang <austinwang@Austins-MBP>
2020-02-04 01:00:48 -0500

    star dispatch

--- run_star.py
+++ run_star.py
@@ -53,6 +53,8 @@ def dispatch_star(bam_map, vcf_map, bed_map, genome_path, boundaries_path, white
         cmd = format_command(k, v, bed_path, vcf_path, genome_path, boundaries_path, whitelist_path, out_prefix)
         jobs.append(cmd)
 
+    print(" & ".join([" ".join(cmd) for cmd in jobs])) ####
+
     for i in jobs:
         while True:
             try:
