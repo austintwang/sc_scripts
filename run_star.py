484b2e488772cb23258edbd10568b9c6b3b03318
Austin Wang <austinwang@Austins-MBP>
2020-02-24 09:26:15 -0500

    kellis data

--- run_star.py
+++ run_star.py
@@ -195,7 +195,7 @@ if __name__ == '__main__':
     # Clean up Kellis
     fail_kellis_48 = get_failed_jobs(kellis_48.keys(), out_path_base_kellis_48)
     dispatch_star(
-        bam_map_kellis_48, vcf_map_kellis_48, bed_map_kellis_48, genome_path, boundaries_path, whitelist_path, out_path_base_kellis_48, 60000
+        bam_map_kellis_48, vcf_map_kellis_48, bed_map_kellis_48, genome_path, boundaries_path, whitelist_path, out_path_base_kellis_48, 60000, selection=fail_kellis_48
     )
 
 
