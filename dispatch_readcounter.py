fa23557f7d8ae2b399c1e4967cc585879fb1ab81
Austin Wang <austinwang@Austins-MBP>
2020-02-29 10:32:49 -0500

    split data into genes

--- dispatch_readcounter.py
+++ dispatch_readcounter.py
@@ -14,7 +14,7 @@ def dispatch(script_path, data_dir, boundaries_path, names, out_pattern_base, me
         status_path = os.path.join(data_dir, name, "countstatus.txt")
         if fails_only and os.path.isfile(status_path):
             with open(status_path) as status_file:
-                if status_file.read() == "Complete\n"
+                if status_file.read() == "Complete\n":
                     continue
 
         err_name = os.path.join(data_dir, name, "count_%j.out")
