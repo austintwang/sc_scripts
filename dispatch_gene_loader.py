cf88ceb0732de12e74ab0173c3c2a608cb1295ae
Austin Wang <austinwang@Austins-MBP>
2020-03-16 16:34:53 -0700

    read mapping

--- dispatch_gene_loader.py
+++ dispatch_gene_loader.py
@@ -18,7 +18,7 @@ def dispatch(script_path, names, dataset_name, radius, data_dir, vcf_path, barco
 
         err_name = os.path.join(data_dir, name, "load_%j.out")
         cmd = [
-            "sbatch", "--mem={0}".format(memory), "-J", name, "-o", err_name,
+            "sbatch", "--mem={0}".format(memory), "-J", name, "-o", err_name, "-x", "node02,node13",
             script_path, name, dataset_name, str(radius), data_dir, vcf_path, barcodes_map_path, boundaries_map_path, tss_map_path, status_path
         ]
         print(" ".join(cmd))
@@ -69,6 +69,20 @@ if __name__ == '__main__':
         tss_map_path, 
         2000, 
         fails_only=False
+    )
+
+    dispatch(
+        script_path, 
+        names_kellis,
+        "Kellis",
+        radius, 
+        genes_dir_kellis, 
+        vcf_path_kellis, 
+        barcodes_map_path_kellis, 
+        boundaries_map_path, 
+        tss_map_path, 
+        2000, 
+        fails_only=True
     )
 
 
