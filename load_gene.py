71b490159d6944378a51b52ee1075b94e9a8d1e1
Austin Wang <austinwang@Austins-MBP>
2020-03-16 13:56:47 -0700

    read mapping

--- load_gene.py
+++ load_gene.py
@@ -35,7 +35,7 @@ def add_data(agg_counts, var_data, cell_map, genotypes, sample_gen_map, marker_g
         for cell, counts in cells.items():
             cell_agg = agg_counts.setdefault(cell, np.array([0,0,0]))
             cell_agg[2] += np.sum(counts)
-            if not (var in markers):
+            if not (var in marker_gen_map):
                 continue
             sample_idx = sample_gen_map[cell_map[cell]]
             marker_idx = marker_gen_map[var]
