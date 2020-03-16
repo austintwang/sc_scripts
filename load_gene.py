4cfa38b21e3176762af4726a79d1116560398ec9
Austin Wang <austinwang@Austins-MBP>
2020-03-16 16:28:47 -0700

    read mapping

--- load_gene.py
+++ load_gene.py
@@ -71,42 +71,43 @@ def load_gene(gene_name, dataset_name, radius, data_dir, vcf_path, barcodes_map_
 
     radius = int(radius)
 
-    contig, start, end = boundaries_map[gene_name]
-    genotypes, samples, markers, marker_ids = read_vcf(vcf_path, contig, start, end)
-    samples = sample_process_fn(samples)
-    sample_gen_map = dict([(val, ind) for ind, val in enumerate(samples)])
-    marker_gen_map = dict([(val, ind) for ind, val in enumerate(markers)])
-    # print(marker_gen_map.keys()) ####
+    if gene_name.split(".")[0] in tss_map:
+        contig, start, end = boundaries_map[gene_name]
+        genotypes, samples, markers, marker_ids = read_vcf(vcf_path, contig, start, end)
+        samples = sample_process_fn(samples)
+        sample_gen_map = dict([(val, ind) for ind, val in enumerate(samples)])
+        marker_gen_map = dict([(val, ind) for ind, val in enumerate(markers)])
+        # print(marker_gen_map.keys()) ####
 
-    contig, tss_pos = tss_map[gene_name.split(".")[0]]
-    contig = contig[3:]
-    genotypes_nc, samples_nc, markers_nc, marker_ids_nc = read_vcf(
-        vcf_path, contig, tss_pos - radius, tss_pos + radius + 1
-    )
-    samples_nc = sample_process_fn(samples_nc)
-    sample_gen_map_nc = dict([(val, ind) for ind, val in enumerate(samples_nc)])
-    marker_gen_map_nc = dict([(val, ind) for ind, val in enumerate(markers_nc)])
-    
-    agg_counts = {}
-    var_data_paths = os.listdir(os.path.join(gene_dir, "bamdata")) 
-    for path in var_data_paths:
-        with open(os.path.join(gene_dir, "bamdata", path), "rb") as var_file:
-            var_data = pickle.load(var_file)
-            add_data(agg_counts, var_data, barcodes_map, genotypes, sample_gen_map, marker_gen_map)
+        contig, tss_pos = tss_map[gene_name.split(".")[0]]
+        contig = contig[3:]
+        genotypes_nc, samples_nc, markers_nc, marker_ids_nc = read_vcf(
+            vcf_path, contig, tss_pos - radius, tss_pos + radius + 1
+        )
+        samples_nc = sample_process_fn(samples_nc)
+        sample_gen_map_nc = dict([(val, ind) for ind, val in enumerate(samples_nc)])
+        marker_gen_map_nc = dict([(val, ind) for ind, val in enumerate(markers_nc)])
+        
+        agg_counts = {}
+        var_data_paths = os.listdir(os.path.join(gene_dir, "bamdata")) 
+        for path in var_data_paths:
+            with open(os.path.join(gene_dir, "bamdata", path), "rb") as var_file:
+                var_data = pickle.load(var_file)
+                add_data(agg_counts, var_data, barcodes_map, genotypes, sample_gen_map, marker_gen_map)
 
-    print(agg_counts) ####
-    # print(genotypes_nc) ####
-    out_data = {
-        "name": gene_name, 
-        "genotypes": genotypes_nc, 
-        "samples": samples_nc, 
-        "markers": markers_nc, 
-        "marker_ids": marker_ids_nc,
-        "cell_counts": agg_counts
-    }
-    out_path = os.path.join(gene_dir, "gene_data.pickle")
-    with open(out_path, "wb") as out_file:
-        pickle.dump(out_data, out_file)
+        print(agg_counts) ####
+        # print(genotypes_nc) ####
+        out_data = {
+            "name": gene_name, 
+            "genotypes": genotypes_nc, 
+            "samples": samples_nc, 
+            "markers": markers_nc, 
+            "marker_ids": marker_ids_nc,
+            "cell_counts": agg_counts
+        }
+        out_path = os.path.join(gene_dir, "gene_data.pickle")
+        with open(out_path, "wb") as out_file:
+            pickle.dump(out_data, out_file)
 
     with open(status_path, "w") as status_file:
         status_file.write("Complete")
