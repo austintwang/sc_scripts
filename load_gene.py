3bd5575405a8d9a0303249f11033529f14caf67f
Austin Wang <austinwang@Austins-MBP>
2020-03-16 11:42:24 -0700

    read mapping

--- load_gene.py
+++ load_gene.py
@@ -20,8 +20,8 @@ def read_vcf(vcf_path, contig, start, end):
             if not val.phased:
                 add_marker = False
                 break
-            record_gens[ind,:] = np.array(val.alleles_indices, dtype=int)
-            print(val.alleles_indices) ####
+            record_gens[ind,:] = np.array(val.allele_indices, dtype=int)
+            print(val.allele_indices) ####
         if add_marker:
             genotypes_list.append(record_gens)
             markers.append((chrom, record.pos,),)
