21fb9d77f4655ea728474101f26f3c61d1164bb5
Austin Wang <austinwang@Austins-MBP>
2020-03-17 22:43:58 -0700

    run colocalization

--- colocalize.py
+++ colocalize.py
@@ -42,9 +42,9 @@ def run_plink_ld(gwas_gen_path, marker_ids, num_snps, contig):
         next(out_file)
         for line in out_file:
             print(line) ####
-            if line == "\n":
-                continue
-            data = line.strip().split("\t")
+            # if line == "\n":
+            #     continue
+            data = line.strip().split()
             print(data) ####
             id1 = data[2]
             id2 = data[5]
