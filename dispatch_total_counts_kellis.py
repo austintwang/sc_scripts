88fd622f691a46e9ef3acaf6c07808e2bac66f28
Austin Wang <austinwang@Austins-MBP>
2020-03-24 15:32:36 -0700

    use total counts

---
+++ dispatch_total_counts_kellis.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import pickle
+import gzip
+import glob
+import numpy as np
+
+
+def dispatch(script_path, names, base_path, rows_path, genes_dir, agg_out_dir, job_data_dir, memory):
+    jobs = []
+    for file_name in names:
+        err_name = os.path.join(job_data_dir, file_name, "load_%j.out")
+        cmd = [
+            "sbatch", "--mem={0}".format(memory), "-J", file_name, "-o", err_name, "-x", "node02,node13",
+            script_path, base_path, rows_path, genes_dir, agg_out_dir
+        ]
+        print(" ".join(cmd))
+        jobs.append(cmd)
+
+    timeout = "sbatch: error: Batch job submission failed: Socket timed out on send/recv operation"
+    for i in jobs:
+        while True:
+            try:
+                submission = subprocess.run(i, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print(str(submission.stdout, 'utf-8').rstrip())
+                break
+            except subprocess.CalledProcessError as e:
+                # print(e.stdout) ####
+                err = str(e.stderr, 'utf-8').rstrip()
+                print(err)
+                if err == timeout:
+                    print("Retrying Submit")
+                    continue
+                else:
+                    raise e
+
+def load_counts(counts_dir, rows_path, genes_dir, agg_out_path):
+    with gzip.open(rows_path, "rb") as row_file:
+        row_names = row_file.read().decode('utf-8').strip().split("\n")
+    counts_paths = glob.glob(os.path.join(counts_dir, "*.s1.gz"))
+    counts_agg = {}
+    for counts_path in counts_paths:
+        base_path = os.path.splitext(os.path.splitext(counts_path)[0])[0]
+        col_path = base_path + ".cols.gz"
+        file_name = os.path.basename(base_path)
+        agg = parse(counts_path, col_path, row_names, genes_dir, file_name)
+        counts_agg.update(agg)
+    with open(agg_out_path, "wb") as agg_out_file:
+        pickle.dump(counts_agg, agg_out_file)
+
+if __name__ == '__main__':
+    curr_path = os.path.abspath(os.path.dirname(__file__))
+    script_path = os.path.join(curr_path, "total_counts_kellis.py")
+
+    base_dir = "/agusevlab/awang/sc_kellis"
+    genes_dir = os.path.join(base_dir, "genes")
+    agg_out_dir = os.path.join(base_dir, "agg_counts")
+
+    data_dir = os.path.join(base_dir, "snRNAseq_PFC_eQTL")
+    counts_dir = os.path.join(data_dir, "result", "aggregate", "merged", "broad")
+    names = [
+        os.path.splitext(os.path.splitext(os.path.basename(i))[0])[0] 
+        for i in glob.glob(os.path.join(counts_dir, "*.s1.gz"))
+    ]
+    rows_path = os.path.join(data_dir, "auxiliary", "all_features.gz")
+
+    job_data_dir = os.path.join(data_dir, "job_data")
+
+    dispatch(script_path, names, counts_dir, rows_path, genes_dir, agg_out_dir, job_data_dir, 2000)
+
--- total_counts_kellis.py
+++ total_counts_kellis.py
@@ -5,10 +5,11 @@ import gzip
 import glob
 import numpy as np
 
-def parse(counts_path, col_path, row_names, out_dir, file_name):
+def parse(counts_path, col_path, row_names, out_dir, agg_out_dir, file_name):
     with gzip.open(col_path, "r") as col_file:
         col_names = col_file.read().decode('utf-8').strip().split("\n")
     counts_agg_arr = np.zeros(len(col_names))
+
     with gzip.open(counts_path, "r") as counts_file:
         for cl, gl in zip(counts_file, row_names):
             counts_lst = list(map(float, cl.decode('utf-8').strip().split(" ")))
@@ -24,31 +25,18 @@ def parse(counts_path, col_path, row_names, out_dir, file_name):
             os.makedirs(gene_counts_dir, exist_ok=True)
             with open(os.path.join(gene_counts_dir, file_name), "wb") as out_file:
                 pickle.dump(counts_dct, out_file)
-    counts_agg_dct = dict(zip(col_names, counts_agg_arr))
-    return counts_agg_dct
 
-def load_counts(counts_dir, rows_path, genes_dir, agg_out_path):
+    counts_agg_dct = dict(zip(col_names, counts_agg_arr))
+    with open(os.path.join(agg_out_dir, file_name), "wb") as agg_out_file:
+            pickle.dump(counts_agg_dct, agg_out_file)
+
+def load_counts(file_name, base_path, rows_path, genes_dir, agg_out_dir):
     with gzip.open(rows_path, "rb") as row_file:
         row_names = row_file.read().decode('utf-8').strip().split("\n")
-    counts_paths = glob.glob(os.path.join(counts_dir, "*.s1.gz"))
-    counts_agg = {}
-    for counts_path in counts_paths:
-        base_path = os.path.splitext(os.path.splitext(counts_path)[0])[0]
-        col_path = base_path + ".cols.gz"
-        file_name = os.path.basename(base_path)
-        agg = parse(counts_path, col_path, row_names, genes_dir, file_name)
-        counts_agg.update(agg)
-    with open(agg_out_path, "wb") as agg_out_file:
-        pickle.dump(counts_agg, agg_out_file)
+    counts_path = os.path.join(base_path, file_name + ".s1.gz")
+    col_path = os.path.join(base_path, file_name + ".cols.gz")
+    parse(counts_path, col_path, row_names, genes_dir, agg_out_dir, file_name)
 
 if __name__ == '__main__':
-    base_dir = "/agusevlab/awang/sc_kellis"
-    genes_dir = os.path.join(base_dir, "genes")
-    agg_out_path = os.path.join(base_dir, "agg_counts.pickle")
-
-    data_dir = os.path.join(base_dir, "snRNAseq_PFC_eQTL")
-    counts_dir = os.path.join(data_dir, "result", "aggregate", "merged", "broad")
-    rows_path = os.path.join(data_dir, "auxiliary", "all_features.gz")
-
-    load_counts(counts_dir, rows_path, genes_dir, agg_out_path)
+    load_counts(*sys.argv[1:])
 
