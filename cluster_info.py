745a0e51a80986ec4440072df63d56c232679c6e
Austin Wang <austinwang@Austins-MBP>
2020-03-25 11:49:01 -0700

    use total counts

--- cluster_info.py
+++ cluster_info.py
@@ -21,6 +21,7 @@ def read_data(plasma_data, clusters, gene_name):
                 gene_name, 
                 c, 
                 plasma_clust["avg_counts_total"],
+                plasma_clust["avg_counts_total_scaled"]
                 plasma_clust["avg_counts_mapped"],
                 np.mean(plasma_clust["overdispersion"]),
                 np.percentile(plasma_clust["overdispersion"], 25),
@@ -67,6 +68,7 @@ def get_info(genes_dir, cluster_map_path, out_path):
         "Gene", 
         "Cluster", 
         "MeanTotalCoverage",
+        "MeanTotalCoverageScaled",
         "MeanMappedCoverage",
         "MeanOverdispersion",
         "25PercOverdispersion",
