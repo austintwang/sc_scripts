09a356cc5cb43ef03981b0ab572bd9860fb62418
Austin Wang <austinwang@Austins-MBP>
2020-03-24 17:41:47 -0700

    use total counts

---
+++ agg_counts_kellis.py
@@ -0,0 +1,19 @@
+import os
+import pickle
+
+def agg_counts(agg_dir, out_path):
+    aggs = {}
+    for path in os.listdir(agg_dir):
+        with open(path, "rb") as agg_file:
+            agg = pickle.load(agg_file)
+        aggs.update(agg)
+
+    with open(out_path, "wb") as out_file:
+        pickle.dump(aggs, out_file)
+
+if __name__ == '__main__':
+    base_dir = "/agusevlab/awang/sc_kellis"
+    agg_dir = os.path.join(base_dir, "agg_counts")
+    out_path = os.path.join(base_dir, "agg_counts.pickle")
+    agg_counts(agg_dir, out_path)
+
