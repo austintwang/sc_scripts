c2196078991481f0e96d66272c6869c97c30f4af
Austin Wang <austinwang@Austins-MBP>
2020-04-09 12:38:44 -0700

    429 samples

--- run_star.py
+++ run_star.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
 
     fail_kellis_429 = get_failed_jobs(bam_map_kellis_429.keys(), out_path_base_kellis_429)
     dispatch_star(
-        bam_map_kellis_429, vcf_map_kellis_429, bed_map_kellis_429, genome_path, boundaries_path, whitelist_path, out_path_base_kellis_429, 40000, selection=fail_kellis_429
+        bam_map_kellis_429, vcf_map_kellis_429, bed_map_kellis_429, genome_path, boundaries_path, whitelist_path, out_path_base_kellis_429, 80000, selection=fail_kellis_429
     )
 
 
