6bc7ad6fd3857d2f1b24a2747a32efc4bdd75dbf
Austin Wang <austinwang@Austins-MBP>
2020-04-12 00:57:00 -0700

    multithread STAR

--- run_star.py
+++ run_star.py
@@ -5,7 +5,7 @@ import subprocess
 import numpy as np
 
 def format_command(job_name, bam_path, bed_path, vcf_path, genome_path, boundaries_path, whitelist_path, out_prefix, paired, memory):
-    threads = str(min(64, 500 // (1400000 // memory)))
+    threads = str(min(64, 400 // (1400000 // memory)))
     star_cmd = [
         "STAR",
         "--runMode", "alignReads",
