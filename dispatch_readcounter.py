26c8bce3d7e19864a04430aedaf787bb6fe2d25c
Austin Wang <austinwang@Austins-MBP>
2020-04-17 16:34:30 -0700

    manual bam parser

--- dispatch_readcounter.py
+++ dispatch_readcounter.py
@@ -24,8 +24,8 @@ def dispatch(script_path, dataset_name, data_dir, boundaries_path, names, out_pa
                     outs = glob.glob(os.path.join(data_dir, name, "count_*.out")) ####
                     with open(max(outs)) as of: ####
                         ol = of.readlines()
-                    print(ol) ####
-                    print(len(ol))
+                    # print(ol) ####
+                    # print(len(ol)) ####
                     if len(ol) == 1:
                         continue
         if not fails_only:
