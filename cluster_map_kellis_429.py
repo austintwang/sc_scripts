ec4b06268897e5870b2c870569a681084cce2a03
Austin Wang <austinwang@Austins-MBP>
2020-04-18 01:19:04 -0700

    429 samples

--- cluster_map_kellis_429.py
+++ cluster_map_kellis_429.py
@@ -2,16 +2,25 @@ import os
 import pickle
 import gzip
 
-def get_cluster_map(clusters_path, out_path):
+def get_cluster_map(clusters_path, well_map_path, out_path):
+    with open(well_map_path, "rb") as well_map_file:
+        well_map = pickle.load(well_map_file)
+    sample_map = {}
+    for k, v in well_map.items():
+        sample_map.setdefault(v, []).append(k)
+
     cluster_map = {"_all": []}
     with gzip.open(clusters_path, "rt", encoding='utf-8') as clusters_file:
         for line in clusters_file:
             cols = line.strip("\n").split(" ")
-            barcode = cols[0].split("_")[0]
+            barcode, sample = cols[0].split("_")
             cluster = cols[1]
-            cluster_map["_all"].append(barcode)
+            wells = sample_map[sample]
             cells = cluster_map.setdefault(cluster, [])
-            cells.append(barcode)
+            for w in wells:
+                cell = (barcode, w)
+                cluster_map["_all"].append(cell)
+                cells.append(cell)
 
     with open(out_path, "wb") as out_file:
         pickle.dump(cluster_map, out_file)
