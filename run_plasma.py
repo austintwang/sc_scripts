ae636717026687f656f00e03ce5de66d13f75115
Austin Wang <austinwang@Austins-MBP>
2020-04-19 00:05:52 -0700

    429 samples

--- run_plasma.py
+++ run_plasma.py
@@ -225,8 +225,8 @@ def run_plasma(name, data_dir, params_path, filter_path, cluster_map_path, barco
             result["avg_num_cells"] = np.nanmean(inputs["num_cells"])
 
             if processed_counts:
-                print(inputs["counts_total"]) ####
-                print(inputs["counts_norm"]) ####
+                # print(inputs["counts_total"]) ####
+                # print(inputs["counts_norm"]) ####
                 inputs["counts_total"] = inputs["counts_total"] * np.mean(inputs["counts_norm"]) / inputs["counts_norm"]
                 result["avg_counts_total_scaled"] = np.nanmean(inputs["counts_total"])
             else:
