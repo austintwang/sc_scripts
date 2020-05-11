e8bcc91551079c98bfb4e0a0ebfd1d69201797fc
Austin Wang <austinwang@Austins-MBP>
2020-05-11 00:20:33 -0700

    processed counts

--- run_plasma.py
+++ run_plasma.py
@@ -220,6 +220,7 @@ def run_plasma(name, data_dir, params_path, filter_path, cluster_map_path, barco
                 inputs = inputs.copy()
                 inputs.update(inputs_all)
                 print(cluster) ####
+                print(inputs["total_counts"].keys()) ####
                 if inputs["total_counts"] and inputs["total_counts"].get(cluster, False):
                     processed_counts = True
                     # print(inputs["total_counts"][cluster]) ####
