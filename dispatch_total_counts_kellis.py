48d11cb87cd50e4fe168a714b85802c9b83bb0f1
Austin Wang <austinwang@Austins-MBP>
2020-05-26 15:29:44 -0700

    batch correction

--- dispatch_total_counts_kellis.py
+++ dispatch_total_counts_kellis.py
@@ -7,14 +7,14 @@ import subprocess
 import numpy as np
 
 
-def dispatch(script_path, names, base_path, rows_path, genes_dir, agg_out_dir, job_data_dir, memory):
+def dispatch(script_path, clusters, base_path, rows_path, genes_dir, agg_out_dir, job_data_dir, memory):
     jobs = []
-    for file_name in names:
+    for file_name, pattern in clusters.items():
         os.makedirs(os.path.join(job_data_dir, file_name), exist_ok=True)
         err_name = os.path.join(job_data_dir, file_name, "load_%j.out")
         cmd = [
             "sbatch", "--mem={0}".format(memory), "-J", file_name, "-o", err_name, "-x", "node02,node13",
-            script_path, file_name, base_path, rows_path, genes_dir, agg_out_dir
+            script_path, file_name, pattern, base_path, rows_path, genes_dir, agg_out_dir
         ]
         print(" ".join(cmd))
         jobs.append(cmd)
@@ -46,17 +46,19 @@ if __name__ == '__main__':
 
     data_dir = os.path.join(base_dir, "snRNAseq_PFC_eQTL")
     counts_dir = os.path.join(data_dir, "result", "aggregate", "merged", "broad")
-    names = [
-        os.path.splitext(os.path.splitext(os.path.basename(i))[0])[0] 
+    names = {
+        (os.path.splitext(os.path.splitext(os.path.basename(i))[0])[0]).split("_")[-1] 
         for i in glob.glob(os.path.join(counts_dir, "*.s1.gz"))
-    ]
+    }
+    clusters = {i: "*_{0}".format(i) for i in names}
+    clusters["_all"] = "*"
     rows_path = os.path.join(data_dir, "auxiliary", "all_features.gz")
 
-    job_data_dir = os.path.join(data_dir, "job_data")
+    job_data_dir = os.path.join(data_dir, "job_data_processed")
 
     # dispatch(script_path, names, counts_dir, rows_path, genes_dir, agg_out_dir, job_data_dir, 2000)
 
     genes_dir = os.path.join(base_dir, "genes_429")
-    dispatch(script_path, names, counts_dir, rows_path, genes_dir, agg_out_dir, job_data_dir, 2000)
+    dispatch(script_path, clusters, counts_dir, rows_path, genes_dir, agg_out_dir, job_data_dir, 2000)
 
 
--- total_counts_kellis.py
+++ total_counts_kellis.py
@@ -15,42 +15,55 @@ def process(counts_arr):
     res = logtrans - pcs @ regs
     return res
 
-def parse(counts_path, col_path, row_names, out_dir, agg_out_dir, file_name):
-    with gzip.open(col_path, "r") as col_file:
-        col_names = col_file.read().decode('utf-8').strip().split("\n")
-    # print(col_names) ####
-    counts_agg_arr = np.zeros(len(col_names))
-    counts_arr = np.zeros(len(col_names), len(row_names))
+def parse(counts_paths, col_paths, row_names, out_dir, agg_out_dir, name):
+    counts_agg_arrs = []
+    counts_arrs = []
+    col_names_all = []
+    for counts_path, col_path in zip(counts_paths, col_paths):
+        with gzip.open(col_path, "r") as col_file:
+            col_names = col_file.read().decode('utf-8').strip().split("\n")
+        # print(col_names) ####
+        counts_agg_arr = np.zeros(len(col_names))
+        counts_arr = np.zeros(len(col_names), len(row_names))
 
-    with gzip.open(counts_path, "r") as counts_file:
-        for i, cl, gl in zip(range(len(row_names)), counts_file, row_names):
-            counts_gene = np.fromiter(map(float, cl.decode('utf-8').strip().split(" ")), float)
-            counts_arr[:, i] = counts_gene
-            counts_agg_arr += counts_gene
+        with gzip.open(counts_path, "r") as counts_file:
+            for i, cl, gl in zip(range(len(row_names)), counts_file, row_names):
+                counts_gene = np.fromiter(map(float, cl.decode('utf-8').strip().split(" ")), float)
+                counts_arr[:, i] = counts_gene
+                counts_agg_arr += counts_gene
 
-    counts_out = process(counts_arr)
+        counts_agg_arrs.append(counts_agg_arr)
+        counts_arrs.append(counts_arr)
+        col_names_all.extend(col_names)
+
+    counts_agg_all = np.concatenate(counts_agg_arrs)
+    counts_all = np.concatenate(counts_arrs , axis=0)
+
+    counts_out = process(counts_all)
     for i, gl in enumerate(row_names):
-        counts_dct = dict(zip(col_names, counts_out[:,i]))
+        counts_dct = dict(zip(col_names_all, counts_out[:,i]))
         gene = gl.strip()
         out_pattern = os.path.join(out_dir, gene + ".*")
         out_match = glob.glob(out_pattern)
         if len(out_match) == 0:
             continue
-        gene_counts_dir = os.path.join(out_match[0], "total_counts")
+        gene_counts_dir = os.path.join(out_match[0], "processed_counts")
         os.makedirs(gene_counts_dir, exist_ok=True)
-        with open(os.path.join(gene_counts_dir, file_name), "wb") as out_file:
+        with open(os.path.join(gene_counts_dir, name), "wb") as out_file:
             pickle.dump(counts_dct, out_file)
 
-    counts_agg_dct = dict(zip(col_names, counts_agg_arr))
+    counts_agg_dct = dict(zip(col_names_all, counts_agg_all))
     with open(os.path.join(agg_out_dir, file_name), "wb") as agg_out_file:
         pickle.dump(counts_agg_dct, agg_out_file)
 
-def load_counts(file_name, base_path, rows_path, genes_dir, agg_out_dir):
+def load_counts(name, pattern, base_path, rows_path, genes_dir, agg_out_dir):
     with gzip.open(rows_path, "rb") as row_file:
         row_names = row_file.read().decode('utf-8').strip().split("\n")
-    counts_path = os.path.join(base_path, file_name + ".s1.gz")
-    col_path = os.path.join(base_path, file_name + ".cols.gz")
-    parse(counts_path, col_path, row_names, genes_dir, agg_out_dir, file_name)
+    counts_paths = glob.glob(os.path.join(base_path, pattern + ".s1.gz"))
+    col_paths = [i.replace(".s1.gz", ".cols.gz") for i in counts_path]
+    print(counts_paths) ####
+    print(col_paths) ####
+    parse(counts_paths, col_paths, row_names, genes_dir, agg_out_dir, name)
 
 if __name__ == '__main__':
     load_counts(*sys.argv[1:])
