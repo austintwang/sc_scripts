bad4422a9bad6bc9237f43f9231cfcaf33d64b5b
Austin Wang <austinwang@Austins-MBP>
2020-05-29 23:30:22 -0700

    sample masks

--- load_gene.py
+++ load_gene.py
@@ -71,8 +71,11 @@ def add_data(agg_counts, var_data, cell_map, genotypes, sample_gen_map, marker_g
 def process_samplename_kellis(sample_names):
     return [i[-8:] for i in sample_names]
 
+# def process_countsname_kellis(counts_names):
+#     return [i.split("_")[-1] for i in counts_names]
+
 def process_countsname_kellis(counts_names):
-    return [i.split("_")[-1] for i in counts_names]
+    return counts_names
 
 def load_gene(gene_name, dataset_name, radius, min_maf, min_info, well_only, ignore_total, data_dir, vcf_path, barcodes_map_path, boundaries_map_path, tss_map_path, total_counts_norm_path, status_path):
     with open(status_path, "w") as status_file:
