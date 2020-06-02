28de930636decbd361ca480c066790b0708579ef
Austin Wang <austinwang@Austins-MBP>
2020-06-01 20:38:21 -0700

    save snp alleles

--- get_twas_inputs.py
+++ get_twas_inputs.py
@@ -11,7 +11,7 @@ def write_gene(gene_name, gene_path_base, out_path_base):
     out_gene_dir = os.path.join(out_path_base, gene_name)
     os.makedirs(out_gene_dir, exist_ok=True)
     print(gene_name) ####
-    print(plasma_data.keys()) ####
+    # print(plasma_data.keys()) ####
     np.savetxt(os.path.join(out_gene_dir, "hapA.txt"), plasma_data["_gen"]["hap_A"], fmt='%i')
     np.savetxt(os.path.join(out_gene_dir, "hapB.txt"), plasma_data["_gen"]["hap_B"], fmt='%i')
     pos = plasma_data["_gen"]["snp_pos"]
