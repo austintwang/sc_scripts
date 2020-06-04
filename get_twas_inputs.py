0b8c98fe64f6551d4fb3966ae28780f4ab8c974b
Austin Wang <austinwang@Austins-MBP>
2020-06-03 19:00:24 -0700

    parallelize twas inputs

--- get_twas_inputs.py
+++ get_twas_inputs.py
@@ -13,7 +13,7 @@ def write_gene(gene_name, gene_path_base, out_path_base):
 
     out_gene_dir = os.path.join(out_path_base, gene_name)
     os.makedirs(out_gene_dir, exist_ok=True)
-    print(gene_name) ####
+    # print(gene_name) ####
     # print(plasma_data.keys()) ####
     np.savetxt(os.path.join(out_gene_dir, "hapA.txt"), plasma_data["_gen"]["hap_A"], fmt='%i')
     np.savetxt(os.path.join(out_gene_dir, "hapB.txt"), plasma_data["_gen"]["hap_B"], fmt='%i')
