b3194ccaa3799bd668cbfac50e45b0b961faaa99
Austin Wang <austinwang@Austins-MBP>
2020-06-05 14:53:37 -0700

    updated gwas

--- get_gwas_data.py
+++ get_gwas_data.py
@@ -9,6 +9,7 @@ def get_gwas_data(gwas_path, out_path):
     with gzip.open(gwas_path, 'rb') as gwas_file:
         next(gwas_file)
         for line in gwas_file:
+            print(line) ####
             data = line.decode('utf-8').split()
             if data[3] == 'Inf':
                 continue
