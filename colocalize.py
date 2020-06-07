264f39ba0566573007f5a970bcb32a1fb5d88918
Austin Wang <austinwang@Austins-MBP>
2020-06-06 22:42:41 -0700

    colocalization

--- colocalize.py
+++ colocalize.py
@@ -217,13 +217,13 @@ def colocalize(gene_name, data_dir, params_path, filter_path, gwas_dir, gwas_gen
                             # snps_used = result["ppas_{0}".format(fg)] != np.nan
                             snps_used = np.logical_not(np.isnan(result["ppas_{0}".format(fg)]))
                             scale = np.nansum(fm_res["ppas_{0}".format(fq)][snps_used])
-                            print(scale) ####
-                            print(snps_used) ####
-                            print(result["ppas_{0}".format(fg)]) ####
+                            # print(scale) ####
+                            # print(snps_used) ####
+                            # print(result["ppas_{0}".format(fg)]) ####
                             fm_res_scaled = fm_res["ppas_{0}".format(fq)] / scale
                             clpps = fm_res_scaled * result["ppas_{0}".format(fg)]
-                            print(fm_res["ppas_{0}".format(fq)]) ####
-                            print(clpps) ####
+                            # print(fm_res["ppas_{0}".format(fq)]) ####
+                            # print(clpps) ####
                             h4 = np.nansum(clpps)
                             cluster_results[cluster]["clpp_{0}_{1}".format(fq, fg)] = clpps
                             cluster_results[cluster]["h4_{0}_{1}".format(fq, fg)] = h4
