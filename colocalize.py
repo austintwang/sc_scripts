f937d0cce5a3cce479a28350a0590f7b2daa5e83
Austin Wang <austinwang@Austins-MBP>
2020-06-07 21:29:53 -0700

    coloc priors

--- colocalize.py
+++ colocalize.py
@@ -231,7 +231,7 @@ def colocalize(gene_name, data_dir, params_path, filter_path, gwas_dir, gwas_gen
                             # if study == "BDSCZ_Ruderfer2018.pickle" and cluster == "Ex":
                                 # print(cluster, fg, fq) ####
                                 # print(fm_res_scaled) ####
-                                print(list(zip(gene_data["marker_ids"], fm_res_scaled, inputs["z_beta"], clpps))) ####
+                                # print(list(zip(gene_data["marker_ids"], fm_res_scaled, inputs["z_beta"], clpps))) ####
                             cluster_results[cluster]["h4_{0}_{1}".format(fq, fg)] = h4
                         except KeyError:
                             continue
