70a453fd47fe2f7f93bf971a3aa56ad9e52e3ccb
Austin Wang <austinwang@Austins-MBP>
2020-06-07 19:40:57 -0700

    coloc priors

--- colocalize.py
+++ colocalize.py
@@ -227,7 +227,8 @@ def colocalize(gene_name, data_dir, params_path, filter_path, gwas_dir, gwas_gen
                             # print(fm_res["ppas_{0}".format(fq)]) ####
                             if study == "Intelligence_SavageJansen2018.pickle":
                                 print(cluster, fg, fq) ####
-                                print(fm_res_scaled) ####
+                                # print(fm_res_scaled) ####
+                                print(zip(fm_res_scaled, inputs["z_beta"])) ####
                             h4 = np.nansum(clpps)
                             cluster_results[cluster]["clpp_{0}_{1}".format(fq, fg)] = clpps
                             cluster_results[cluster]["h4_{0}_{1}".format(fq, fg)] = h4
