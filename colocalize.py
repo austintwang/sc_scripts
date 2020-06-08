3ed9aa61150387099500f56294e6973f1b3b7fb6
Austin Wang <austinwang@Austins-MBP>
2020-06-07 21:12:11 -0700

    coloc priors

--- colocalize.py
+++ colocalize.py
@@ -228,7 +228,7 @@ def colocalize(gene_name, data_dir, params_path, filter_path, gwas_dir, gwas_gen
                             # print(fm_res["ppas_{0}".format(fq)]) ####
                             if study == "BDSCZ_Ruderfer2018.pickle":
                                 print(cluster, fg, fq) ####
-                                print(fm_res_scaled) ####
+                                # print(fm_res_scaled) ####
                                 print(list(zip(gene_data["marker_ids"], gene_data["markers"], fm_res_scaled, inputs["z_beta"]))) ####
                             h4 = np.nansum(clpps)
                             cluster_results[cluster]["clpp_{0}_{1}".format(fq, fg)] = clpps
