6c5c16c23f1d8942f594590c5fea20de6660d577
Austin Wang <austinwang@Austins-MBP>
2020-06-09 19:04:12 -0700

    bulk qtl

--- run_plasma_bulk.py
+++ run_plasma_bulk.py
@@ -170,7 +170,7 @@ def colocalize(gene_name, bulk_name, data_dir, params_path, filter_path, boundar
                         clpps = fm_res_scaled * result["ppas_{0}".format(fg)]
                         h4 = np.nansum(clpps)
                         print(cluster, fg, fq) ####
-                        print(list(zip(clpps, fm_res_scaled, result["ppas_{0}".format(fg)]))) ####
+                        print(sorted(list(zip(clpps, fm_res_scaled, result["ppas_{0}".format(fg)])), key=lambda x: np.nan_to_num(-x[2]))) ####
                         cluster_results[cluster]["clpp_{0}_{1}".format(fq, fg)] = clpps
                         cluster_results[cluster]["h4_{0}_{1}".format(fq, fg)] = h4
                     except KeyError:
