b968f6f618995d59006e262675104d73d87b4748
Austin Wang <austinwang@Austins-MBP>
2020-06-15 19:38:21 -0700

    get cell-type-specific loci

--- cluster_info.py
+++ cluster_info.py
@@ -403,7 +403,7 @@ def plot_sets(df, out_dir):
         df_merged["TopSNPZCombDiff"] = np.abs(df_merged["TopSNPZComb_clust"] - df_merged["TopSNPZComb_all"])
         cutoff = int(len(df_merged) * 0.1)
         # print(df_merged.columns) ####
-        df_specific = df_merged.loc[:,("Gene", "TopSNPZCombDiff", "TopSNPZComb_clust", "TopSNPZComb_all")].iloc[cutoff,:]
+        df_specific = df_merged.loc[:,("Gene", "TopSNPZCombDiff", "TopSNPZComb_clust", "TopSNPZComb_all")].iloc[:cutoff]
         df_specific.to_csv(os.path.join(out_dir, "cell_type_spec", f"{cluster}.csv"), sep="\t", index=False, na_rep="None")   
 
         data_spec = [
