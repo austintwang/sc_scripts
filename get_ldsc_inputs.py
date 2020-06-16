711c4f122511ad4b16cd9bc57a2d4d4759f72da9
Austin Wang <austinwang@Austins-MBP>
2020-06-16 00:54:01 -0700

    get cell-type-specific loci

--- get_ldsc_inputs.py
+++ get_ldsc_inputs.py
@@ -32,81 +32,28 @@ def load_gene(data, cluster, gene, genes_dir):
         ppa = plasma_clust["ppa_indep"][ind]
         data.append([contig, pos, pos + 1, rsid, ppa, gene])
 
-def load_cluster(data, cluster_path, genes_dir):
+def write_bed(data, out_path):
+    with open(out_path, "w") as out_file:
+        out_file.writelines(f"{" ".join(map(str, i))}\n" for i in data)
+
+def load_cluster(cluster, clusters_dir, genes_dir, out_dir):
+    cluster_path = os.path.join(clusters_dir, f"{cluster}.csv")
     df = pd.read_csv(cluster_path)
+    data = []
     for gene in df["Gene"]:
+        load_gene(data, cluster, gene, genes_dir)
 
+    out_path = os.path.join(out_dir, cluster)
+    write_bed(data, out_path)
 
-
-def write_gene(gene_name, gene_path_base, out_path_base):
-    gene_path = os.path.join(gene_path_base, gene_name)
-    plasma_path = os.path.join(gene_path, "combined", "plasma_i0.pickle") # Update after plasma rerun
-    with open(plasma_path, "rb") as plasma_file:
-        plasma_data = pickle.load(plasma_file)
-
-    out_gene_dir = os.path.join(out_path_base, gene_name)
-    os.makedirs(out_gene_dir, exist_ok=True)
-    # print(gene_name) ####
-    # print(plasma_data.keys()) ####
-    np.savetxt(os.path.join(out_gene_dir, "hapA.txt"), plasma_data["_gen"]["hap_A"], fmt='%i')
-    np.savetxt(os.path.join(out_gene_dir, "hapB.txt"), plasma_data["_gen"]["hap_B"], fmt='%i')
-    pos = plasma_data["_gen"]["snp_pos"]
-    # print(pos) ####
-    sid = plasma_data["_gen"]["snp_ids"]
-    sal = plasma_data["_gen"]["snp_alleles"]
-    snp_data = np.stack(
-        [np.array([i[0][0], int(i[0][1]) + 1, i[1], i[2][0], i[2][1]], dtype='object') for i in zip(pos, sid, sal)],
-    )
-    np.savetxt(os.path.join(out_gene_dir, "snps.txt"), snp_data, fmt="%s")
-
-    for cluster, result in plasma_data.items():
-        cluster_dir = os.path.join(out_gene_dir, cluster)
-        if cluster == "_gen":
-            continue
-        # print(cluster) ####
-        # print(result.keys()) ####
-        # print(result) ####
-        try:
-            counts_A = result["counts_A"]
-            counts_B = result["counts_B"]
-            total_exp = result["total_exp"]
-            errs = np.sqrt(result["imbalance_errors"])
-        except KeyError:
-            continue
-
-        os.makedirs(cluster_dir, exist_ok=True)
-        # for i in os.listdir(cluster_dir):
-        #     os.remove(os.path.join(cluster_dir, i))
-        np.savetxt(os.path.join(cluster_dir, "countsA.txt"),  counts_A)
-        np.savetxt(os.path.join(cluster_dir, "countsB.txt"), counts_B)
-        np.savetxt(os.path.join(cluster_dir, "countsTotal.txt"), total_exp)
-        np.savetxt(os.path.join(cluster_dir, "sampleErr.txt"), errs)
-
-def get_twas_inputs(gene, gene_path_base, out_path_base, status_path):
-    with open(status_path, "w") as status_file:
-        status_file.write("")
-
-    try:
-        write_gene(gene, gene_path_base, out_path_base)
-    except FileNotFoundError:
-        pass
-
-    with open(status_path, "w") as status_file:
-        status_file.write("Complete")
+def get_ldsc_inputs(clusters_dir, genes_dir, out_dir):
+    for cluster in os.listdir(clusters_dir):
+        load_cluster(cluster, clusters_dir, genes_dir, out_dir)
 
 if __name__ == '__main__':
-    get_twas_inputs(*sys.argv[1:])
+    data_path_kellis = "/agusevlab/awang/sc_kellis"
+    genes_dir = os.path.join(data_path_kellis, "genes_429")
+    out_dir = os.path.join(data_path_kellis, "ldsc_429")
+    clusters_dir = "/agusevlab/awang/ase_finemap_results/sc_results/kellis_429/cell_type_spec"
+    get_ldsc_inputs(clusters_dir, genes_dir, out_dir)
 
-
-# def get_twas_inputs(gene_path_base, out_path_base):
-#     for gene in os.listdir(gene_path_base):
-#         try:
-#             write_gene(gene, gene_path_base, out_path_base)
-#         except FileNotFoundError:
-#           continue
-
-# if __name__ == '__main__':
-#     data_path_kellis = "/agusevlab/awang/sc_kellis"
-#     genes_dir_kellis = os.path.join(data_path_kellis, "genes_429")
-#     out_dir_kellis = os.path.join(data_path_kellis, "twas_429")
-#     get_twas_inputs(genes_dir_kellis, out_dir_kellis)
