055024cc08bbaa6a7df2165b11ba7193cd7a3594
Austin Wang <austinwang@Austins-MBP>
2020-06-17 18:07:19 -0700

    fdr cross validation

--- cluster_info.py
+++ cluster_info.py
@@ -407,10 +407,13 @@ def plot_sets(df, out_dir):
             suffixes=["_clust", "_all"]
         )
         df_merged["TopSNPZ2CombDiff"] = df_merged["TopSNPZComb_clust"]**2 - df_merged["TopSNPZComb_all"]**2
-        cutoff = int(len(df_merged) * 0.1)
+        threshs = [0.1, 0.2, 0.5]
         # print(df_merged.columns) ####
-        df_specific = df_merged.loc[:,("Gene", "TopSNPZ2CombDiff", "TopSNPZComb_clust", "TopSNPZComb_all")].sort_values(by="TopSNPZ2CombDiff", ascending=False).iloc[:cutoff]
-        df_specific.to_csv(os.path.join(out_dir, "cell_type_spec", f"{cluster}.csv"), sep="\t", index=False, na_rep="None")   
+        df_specific = df_merged.loc[:,("Gene", "TopSNPZ2CombDiff", "TopSNPZComb_clust", "TopSNPZComb_all")].sort_values(by="TopSNPZ2CombDiff", ascending=False)
+        .iloc[:cutoff]
+        for ind, thresh in enumerate(threshs:):
+            cutoff = int(len(df_merged) * thresh)
+            df_specific.iloc[:cutoff].to_csv(os.path.join(out_dir, "cell_type_spec", f"{cluster}_{thresh}.csv"), sep="\t", index=False, na_rep="None")   
 
         data_spec = [
             np.count_nonzero(np.logical_and(
