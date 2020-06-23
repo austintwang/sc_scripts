5a6cbe3efa219b11ee95a6105d52036d36e64ad5
Austin Wang <austinwang@Austins-MBP>
2020-06-23 14:03:48 -0700

    plot coloc loci

--- colocalize.py
+++ colocalize.py
@@ -205,9 +205,10 @@ def colocalize(gene_name, data_dir, params_path, filter_path, gwas_dir, gwas_gen
 
             inputs["corr_shared"], gwas_alleles = run_plink_ld(gwas_gen_path, inputs["snp_ids"], inputs["num_snps"], contig)
             # print(inputs["corr_shared"]) ####
-            print(len(gwas_alleles), len(inputs["snp_alleles"]), len(inputs["z_beta"])) ####
-            alleles_diff = np.fromiter((q[0] == g[0] for q, g in zip(inputs["snp_alleles"], gwas_alleles)), bool)
-            result["z_beta"] = inputs["z_beta"] * alleles_diff
+            # print(len(gwas_alleles), len(inputs["snp_alleles"]), len(inputs["z_beta"])) ####
+            alleles_diff = np.fromiter((q[0] == g[0] for q, g in zip(inputs["snp_alleles"][informative_snps], gwas_alleles)), bool)
+            result["z_beta"] = inputs["z_beta"].copy()
+            result["z_beta"][informative_snps] *= alleles_diff
 
             if inputs["model_flavors_gwas"] == "all":
                 model_flavors_gwas = set(["eqtl", "fmb"])
