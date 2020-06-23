806ceaf566892911c25314441049f08817918f6d
Austin Wang <austinwang@Austins-MBP>
2020-06-23 14:01:03 -0700

    plot coloc loci

--- colocalize.py
+++ colocalize.py
@@ -205,6 +205,7 @@ def colocalize(gene_name, data_dir, params_path, filter_path, gwas_dir, gwas_gen
 
             inputs["corr_shared"], gwas_alleles = run_plink_ld(gwas_gen_path, inputs["snp_ids"], inputs["num_snps"], contig)
             # print(inputs["corr_shared"]) ####
+            print(len(gwas_alleles), len(inputs["snp_alleles"]), len(inputs["z_beta"])) ####
             alleles_diff = np.fromiter((q[0] == g[0] for q, g in zip(inputs["snp_alleles"], gwas_alleles)), bool)
             result["z_beta"] = inputs["z_beta"] * alleles_diff
 
