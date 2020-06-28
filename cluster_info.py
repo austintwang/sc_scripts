c1dd61702f717b3810ff5d1cda260fb7394d4bff
Austin Wang <austinwang@Austins-MBP>
2020-06-27 17:33:07 -0700

    upset plot

--- cluster_info.py
+++ cluster_info.py
@@ -258,7 +258,9 @@ def make_thresh_barplot(
 def make_upset_plot(df, st, sn, thresh, clusters, result_path):
     df_select = df.loc[df[f"TopSNP{st}{sn}"] >= -np.log10(thresh), :]
     sets_data = {}
+    print(df_select) ####
     for i in df_select:
+        print(i) ####
         sets_data.setdefault(i["Gene"], set()).add(i["Cluster"])
     counts = {}
     for key, value in sets_data.items():
