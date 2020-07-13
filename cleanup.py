47cd9476d1f131814fe11872953668360e8e7a62
Austin Wang <austinwang@Austins-MBP>
2020-07-12 20:49:41 -0700

    ldsc total expression

---
+++ cleanup.py
@@ -0,0 +1,17 @@
+import os
+import glob
+
+def cleanup(genes_dir, wildcards):
+	for i in os.listdir(genes_dir):
+		for w in wildcards:
+			rm = os.path.join(genes_dir, i, w)
+			print(rm)
+			os.remove(rm)
+
+if __name__ == '__main__':
+    # Kellis 429
+    data_path_kellis = "/agusevlab/awang/sc_kellis"
+    genes_dir_kellis = os.path.join(data_path_kellis, "genes_429")
+    wildcards = ["*.out"]
+
+    cleanup(genes_dir_kellis, wildcards)
