1acc60c9aa5220215bf7768c05f84a10ac29041d
Austin Wang <austinwang@Austins-MBP>
2020-07-19 10:01:32 -0700

    debug contigs

--- cluster_info.py
+++ cluster_info.py
@@ -506,6 +506,7 @@ def make_scatter(
     plt.clf()
 
 def plot_xval(df, out_dir):
+    os.makedirs(out_dir, exist_ok=True)
     clusters = {
         "_all": "All Cells",
         "Ex": "Excitatory Neuron",
@@ -556,6 +557,7 @@ def make_heatmap(arr, order, title, result_path):
     plt.clf()
 
 def plot_xcells(df_train, df_test, out_dir, stat_name_train, stat_name_test, cutoff_train, cutoff_test):
+    os.makedirs(out_dir, exist_ok=True)
     sn1 = stat_name_train
     sn2 = stat_name_test
     df_tr_sig = df_train.loc[
@@ -716,7 +718,6 @@ def get_info_xval(run_name, num_splits, genes_dir, cluster_map_path, out_dir, gl
     # print(df_train) ####
     # print(df_test) ####
     # print(df_comb) ####
-    os.makedirs(os.path.join(out_dir, "xvals"), exist_ok=True)
     plot_xval(df_comb, os.path.join(out_dir, "xvals"))
     plot_xcells(df_train, df_test, os.path.join(out_dir, "xcells_phi"), "Phi", "Phi", 5, 5)
     plot_xcells(df_train, df_test, os.path.join(out_dir, "xcells_beta"), "Beta", "Beta", 30, 30)
