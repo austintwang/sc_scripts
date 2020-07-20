4c1303bcd698583e20ffb2a5da8a9154badcefb7
Austin Wang <austinwang@Austins-MBP>
2020-07-19 18:18:34 -0700

    debug contigs

--- vcf_to_bed.py
+++ vcf_to_bed.py
@@ -2,11 +2,14 @@ import os
 import pysam
 
 def convert(in_path, out_path):
+    contig_freqs = {} ####
     vcf_in = pysam.VariantFile(in_path)
     with open(out_path, "w") as bam_out:
         for rec in vcf_in.fetch():
             line = "{0}\t{1}\t{2}\n".format(rec.contig, rec.pos, rec.pos+1)
             bam_out.write(line)
+            contig_freqs.setdefault(rec.contig, 0) ####
+            contig_freqs[rec.contig] += 1 ####
 
 if __name__ == '__main__':
     genotypes_dir = "/agusevlab/awang/sc_le/genotypes/"
