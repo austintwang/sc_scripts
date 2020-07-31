bc9f7a9ae3e7a23e53cb16c105e4365c9264316d
Austin Wang <austinwang@Austins-MBP>
2020-07-30 17:11:32 -0700

    rebuild vcf

--- bam_stream.py
+++ bam_stream.py
@@ -7,7 +7,7 @@ import subprocess
 def stream(contig, bed_path, bam_path):
     print("stream start", file=sys.stderr)
     cmd = ["samtools", "view", "-h", "-L", bed_path, bam_path, str(contig)]
-    subprocess.run(cmd, check=True)
+    subprocess.run(cmd)
 
 if __name__ == '__main__':
     stream(*sys.argv[1:])
