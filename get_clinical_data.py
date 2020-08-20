dd65a6723d224f4c16af5cc26aeb31be090f6433
Austin Wang <austinwang@Austins-MBP>
2020-08-20 13:07:26 -0700

    clinical data

--- get_clinical_data.py
+++ get_clinical_data.py
@@ -1,11 +1,23 @@
 import os
 import pickle
+import operator
 import pandas as pd
+
+OP_MAP = {
+    "lt": operator.lt,
+    "le": operator.le,
+    "eq": operator.eq,
+    "ne": operator.ne,
+    "ge": operator.ge,
+    "gt": operator.gt,
+}
 
 def build_sets(data, categories):
     clinical_sets = {}
-    for name, trait, val in categories:
-        ids = set(data.loc[data[trait] == val]["projid"].astype(str))
+    for name, criteria in categories:
+        evals = [OP_MAP[op](data[trait], val) for trait, val, op in criteria]
+        idx = np.all(evals, axis=0)
+        ids = set(data.loc[idx, "projid"].astype(str))
         # print(data.loc[data[trait] == val]["projid"].astype(str)) ####
         # print(type(ids.pop())) ####
         clinical_sets[name] = ids
@@ -20,8 +32,8 @@ def get_clinical_data(data_path, categories, out_path):
 if __name__ == '__main__':
     data_path = "/agusevlab/awang/sc_kellis/rosmap_clinical/ROSMAP_clinical.csv"
     categories = [
-        ("Female", "msex", 0),
-        ("Male", "msex", 1)
+        ("Female", (("msex", 0, "eq"),)),
+        ("Male", (("msex", 1, "eq"),)),
     ]
     out_path = "/agusevlab/awang/sc_kellis/rosmap_clinical/clinical_sets.pickle"
     get_clinical_data(data_path, categories, out_path)
