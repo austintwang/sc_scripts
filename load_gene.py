98633edadd90763067535dcd027141e3ebd90f85
Austin Wang <austinwang@Austins-MBP>
2020-08-20 12:33:22 -0700

    clinical data

--- load_gene.py
+++ load_gene.py
@@ -128,8 +128,9 @@ def load_gene(gene_name, dataset_name, radius, min_maf, min_info, well_only, ign
         # print(samples_nc) ####
         # sample_gen_map_nc = dict([(val, ind) for ind, val in enumerate(samples_nc)])
         # marker_gen_map_nc = dict([(val, ind) for ind, val in enumerate(markers_nc)])
-        print(genotypes_nc.shape())
-        print(samples_nc.shape())
+        print(genotypes) ####
+        print(genotypes_nc.shape()) ####
+        print(samples_nc.shape()) ####
 
         clinical_masks = {}
         for k, v in clinical_sets.items():
