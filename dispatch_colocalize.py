b89ef45614010f18dfa7d9f5674d68f4a0042490
Austin Wang <austinwang@Austins-MBP>
2020-09-09 20:08:22 -0700

    clinical group colocalization

--- dispatch_colocalize.py
+++ dispatch_colocalize.py
@@ -163,19 +163,19 @@ if __name__ == '__main__':
     params_kellis_all["run_name_plasma"] = "combined_coloc"
     params_kellis_all["run_name_coloc"] = "coloc"
 
-    dispatch(
-        script_path, 
-        names_kellis, 
-        genes_dir_kellis, 
-        params_kellis_all, 
-        params_path_kellis, 
-        "all", 
-        gwas_dir,
-        gwas_gen_path,
-        boundaries_map_path,
-        2000, 
-        fails_only=False
-    )
+    # dispatch(
+    #     script_path, 
+    #     names_kellis, 
+    #     genes_dir_kellis, 
+    #     params_kellis_all, 
+    #     params_path_kellis, 
+    #     "all", 
+    #     gwas_dir,
+    #     gwas_gen_path,
+    #     boundaries_map_path,
+    #     2000, 
+    #     fails_only=False
+    # )
 
     groups = [
         "Female",
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     ]
     # groups = groups[4:] ####
 
-    names_test_path = os.path.join(data_path_kellis, "list_429_test_1.pickle")
+    names_test_path = os.path.join(data_path_kellis, "list_429_sig.pickle")
     with open(names_test_path, "rb") as names_test_file:
         names_test = pickle.load(names_test_file)
 
@@ -201,19 +201,19 @@ if __name__ == '__main__':
         params_kellis_group["run_name_plasma"] = f"clinical_coloc_{group}"
         params_kellis_group["run_name_coloc"] = f"clinical_coloc_res_{group}"
 
-        # dispatch(
-        #     script_path, 
-        #     names_test, 
-        #     genes_dir_kellis, 
-        #     params_kellis_group, 
-        #     params_path_kellis_group, 
-        #     "all", 
-        #     gwas_dir,
-        #     gwas_gen_path,
-        #     boundaries_map_path,
-        #     2000, 
-        #     fails_only=False
-        # )
+        dispatch(
+            script_path, 
+            names_test, 
+            genes_dir_kellis, 
+            params_kellis_group, 
+            params_path_kellis_group, 
+            "all", 
+            gwas_dir,
+            gwas_gen_path,
+            boundaries_map_path,
+            2000, 
+            fails_only=False
+        )
 
 
 
