b6b7a2566839075dc348e5d16eeb821cbde3c4c6
Austin Wang <austinwang@Austins-MBP>
2020-09-10 21:23:18 -0700

    clinical group colocalization

--- run_plasma.py
+++ run_plasma.py
@@ -269,7 +269,7 @@ def run_plasma(name, data_dir, params_path, filter_path, cluster_map_path, barco
                     clinical_mask = inputs.get("sample_masks", {}).get(inputs.get("clinical_group"), True)
                     # print(clinical_mask) ####
                     print(np.logical_and.reduce([
-                        clinical_mask,
+                        # clinical_mask,
                         inputs["counts1"] >= 1,
                         inputs["counts2"] >= 1,
                     ], axis=0)) ####
