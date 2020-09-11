4709d2807677ec4ccb6dda84a888adac5ab549fa
Austin Wang <austinwang@Austins-MBP>
2020-09-10 21:22:28 -0700

    clinical group colocalization

--- run_plasma.py
+++ run_plasma.py
@@ -270,7 +270,8 @@ def run_plasma(name, data_dir, params_path, filter_path, cluster_map_path, barco
                     # print(clinical_mask) ####
                     print(np.logical_and.reduce([
                         clinical_mask,
-                        inputs["counts1"] >= 1, 
+                        inputs["counts1"] >= 1,
+                        inputs["counts2"] >= 1,
                     ], axis=0)) ####
 
                     inputs["mask_imbalance"] = mask_imbalance = np.logical_and.reduce([
