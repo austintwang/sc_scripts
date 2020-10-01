fdb83666a5f4777d4395521dba82c214584475b4
Austin Wang <austinwang@Austins-MBP>
2020-09-30 19:39:33 -0700

    ldsc

--- ldsc_interpret.py
+++ ldsc_interpret.py
@@ -8,7 +8,7 @@ import pickle
 import matplotlib
 matplotlib.use('Agg')
 matplotlib.rcParams['agg.path.chunksize'] = 10000
-import matplotlib.pyplot as pltUlcerative colitis
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
