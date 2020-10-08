2f8a55e605f793e60627a31bb21de1499cf1da6d
Austin Wang <austinwang@Austins-MBP>
2020-10-07 17:20:06 -0700

    gene plot

--- gene_interpret.py
+++ gene_interpret.py
@@ -65,7 +65,7 @@ def calc_fractions(gene_id, rsid, gene_data, finemap_data, gene_map, out_dir):
         direction = np.sign(z_scr)
         prop_eff = (prop_hets * direction) % 1
         prop_eff[::-1].sort()
-        print(direction) ####
+        # print(direction) ####
         allele_eff = alleles[(1-direction)//2]
 
         cluster_name = CLUSTERS[cluster]
@@ -85,7 +85,7 @@ def gene_interpret(genes, data_dir, genes_map_path, run_name_plasma, out_dir):
         with open(finemap_path, "rb") as finemap_file:
             finemap_data = pickle.load(finemap_file)
 
-        top_hit = np.argmax(finemap_data["_all"]["z_phi"]**2)
+        top_hit = np.nanargmax(finemap_data["_all"]["z_phi"]**2)
         top_rsid = finemap_data["_gen"]["snp_ids"][top_hit]
         out_dir_frac = os.path.join(out_dir, "fractions")
         os.makedirs(out_dir_frac, exist_ok=True)
