141dd56a8933e150fca7870d76ce337093606555
Austin Wang <austinwang@Austins-MBP>
2021-04-23 13:00:39 -0700

    twas

--- twas_interpret_2.py
+++ twas_interpret_2.py
@@ -66,7 +66,7 @@ def get_genename(eid, namemap):
 def plot_heatmap(df, title, result_path, var):
     df_hits = df.pivot_table(index="Gene Name", columns="Cluster", values="Hits", aggfunc=np.sum).sort_index()
     sigs = (df_hits.sum(axis=1) > 0)
-    df_plot = df.pivot_table(index="Gene Name", columns="Cluster", values=var, fill_value=np.nan).sort_index()[sigs]
+    df_plot = df.pivot_table(index="Gene Name", columns="Cluster", values=var, fill_value=np.nan, aggfunc='first').sort_index()[sigs]
     # print(df_plot) ####
     # df_plot = df_plot.reindex(STUDY_ORDER)
     # df_plot.rename(index=STUDY_NAMES, inplace=True)
